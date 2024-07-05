63a8952dbb17005af47f8617ec90f0176b323c34
Reilly Spitzfaden <reillypascal@gmail.com>
2024-07-05 13:39:12 -0400

    initial commit

---
+++ Source/MinMaxBin.h
@@ -0,0 +1,15 @@
+/*
+  ==============================================================================
+
+    MinMaxBin.h
+    Created: 5 Jul 2024 1:29:07pm
+    Author:  Reilly Spitzfaden
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "LFO.h"
+#include "Utilities.h"
